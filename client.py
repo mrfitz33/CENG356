e77403c8642da44bf45d5e92caa2422c8ebf223e
Kunal Saxena <ksaxena@uvic.ca>
2016-11-18 16:51:29 -0800

    Build 3: The server generates a random 4 digit number and sends it to the client to be displayed

--- client.py
+++ client.py
@@ -2,12 +2,33 @@
 
 import socket
 
-s = socket.socket()
-host = socket.gethostname()
-port = 59426
+def main():
+	# Create socket and its parameters
+	s = socket.socket()
+	host = socket.gethostname()
+	port = 59426
 
-s.connect((host, port))
-print(s.recv(1024))
-while True:
-	pass
-s.close()
+	# Connect to the server
+	s.connect((host, port))
+
+	# Msg to start winning number generation
+	s.send("start generating winning number")
+
+	# Create a list for the digits of the winning number
+	winning_num = []
+	
+	#print(s.recv(1024))
+
+	while True:
+		if s.recv(1024) == "ready to send digit":
+			s.send("ready to receive digit")
+			winning_num.append(int(s.recv(1024)))
+			print(''.join(str(winning_num)))
+			s.send("received digit")	
+		
+	# Close the connection
+	s.close()
+
+if __name__ == "__main__":
+	main()
+
--- server.py
+++ server.py
@@ -1,16 +1,43 @@
 #!/usr/bin/env python
 
 import socket
+import random
 
-s = socket.socket()
-host = socket.gethostname()
-port = 59426
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-s.bind((host, port))
+def main():
+	# Create socket and its parameters
+	s = socket.socket()
+	host = socket.gethostname()
+	port = 59426
 
-s.listen(5)
-while True:
-	c, addr = s.accept()
-	print "Got connection from", addr
-	c.send('Thank you for connecting')
+	# Allow port to be reused after ending execution
+	s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+	# Bind to the port
+	s.bind((host, port))
+
+	# Wait for client connection
+	s.listen(5)
+
+	# Create a list for the digits of the winning number
+	winning_num = []
+
+	while True:
+		# Establish connection with client
+		c, addr = s.accept()
+		print "Connected to client"
+
+		if c.recv(1024) == "start generating winning number":
+			print "Sending to client"
+			for i in range(0, 4):
+				c.send("ready to send digit")
+				if c.recv(1024) == "ready to receive digit":
+					winning_num.append(random.randint(0, 9))
+					c.send(str(winning_num[i]))
+					if c.recv(1024) == "received digit":
+						pass 
+
+	# Close the connection
 	c.close()
+
+if __name__ == "__main__":
+	main()
